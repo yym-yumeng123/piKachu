754752fe89dd8150a76e4e06924cae29cc8e9171
yym <1614527443@qq.com>
2018-02-02 12:32:49 +0800

    ok

--- piKachu.js
+++ piKachu.js
@@ -1,18 +1,37 @@
 !function(){
+	let duration = 50
+	$('.actions').on('click', 'button', function(e){
+    let $button = $(e.currentTarget) // button
+    let speed = $button.attr('data-speed')
+    $button.addClass('active')
+    .siblings('.active').removeClass('active')
+    switch(speed){
+    	case 'slow':
+    	duration = 100
+    	break
+    	case 'normal':
+    	duration = 50
+    	break
+    	case 'fast':
+    	duration = 10
+    	break
+    }
+})
 	// 前缀 代码 回调
 	function writeCode(prefix, code, fn){
 		let container = document.querySelector("#code")
 		let styleTag = document.querySelector("#styleTag")
 
 		let n = 0
-		let id = setInterval( () => {
+		setTimeout(function run(){
 			n += 1
 			container.innerHTML = code.substring(0, n)
 			styleTag.innerHTML = code.substring(0, n)
 			container.scrollTop = container.scrollHeight
 
-			if (n >= code.length) {
-				window.clearInterval(id)
+			if (n < code.length) {
+				setTimeout(run, duration)
+			}else{
 				fn && fn.call()
 			}
 		},20)
@@ -175,4 +194,5 @@
  */
 	`
 	writeCode('', code)
+
 }.call()
